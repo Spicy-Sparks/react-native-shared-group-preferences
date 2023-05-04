96ae877a9eb4031dbf43f0a70c32db4e17179936
Andrea Morandi <79252959+andreamorandi@users.noreply.github.com>
2023-05-04 15:24:11 +0200

    Added JS event listeners

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 import { NativeModules, Platform } from 'react-native'
 const { RNReactNativeSharedGroupPreferences } = NativeModules
+const emitter = new NativeEventEmitter(RNReactNativeSharedGroupPreferences)
+
+if (Platform.OS === 'android')
+  RNReactNativeSharedGroupPreferences.registerEvents()
 
 export default class SharedGroupPreferences {
 
+  static addEventListener = function (event, listener) {
+    return emitter.addListener(event, listener)
+  }
+
+  static removeEventListener = function (listener) {
+    return emitter.removeSubscription(listener)
+  }
+
+  static removeAllListeners = function (event) {
+    return emitter.removeAllListeners(event)
+  }
+
   static async isAppInstalledAndroid(packageName) {
-    return new Promise((resolve, reject)=>{
-      RNReactNativeSharedGroupPreferences.isAppInstalledAndroid(packageName, installed=>{
+    return new Promise((resolve, reject) => {
+      RNReactNativeSharedGroupPreferences.isAppInstalledAndroid(packageName, installed => {
+        if (installed) {
+          resolve()
+        } else {
+          reject()
+        }
+      })
+    })
+  }
+
+  static async isAppInstalledAndroid(packageName) {
+    return new Promise((resolve, reject) => {
+      RNReactNativeSharedGroupPreferences.isAppInstalledAndroid(packageName, installed => {
         if (installed) {
           resolve()
         } else {
@@ -16,13 +44,13 @@ export default class SharedGroupPreferences {
   }
 
   static async getItem(key, appGroup, inputOptions) {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
       if ((Platform.OS != 'ios') && (Platform.OS != 'android')) {
         reject(Platform.OS)
       }
 
       const options = inputOptions || {}
-      RNReactNativeSharedGroupPreferences.getItem(key, appGroup, options, (errorCode, item)=>{
+      RNReactNativeSharedGroupPreferences.getItem(key, appGroup, options, (errorCode, item) => {
         if (errorCode != null) {
           reject(errorCode)
         } else {
@@ -33,13 +61,13 @@ export default class SharedGroupPreferences {
   }
 
   static async setItem(key, value, appGroup, inputOptions) {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
       if ((Platform.OS != 'ios') && (Platform.OS != 'android')) {
         reject(Platform.OS)
       }
 
       const options = inputOptions || {}
-      RNReactNativeSharedGroupPreferences.setItem(key, JSON.stringify(value), appGroup, options, errorCode=>{
+      RNReactNativeSharedGroupPreferences.setItem(key, JSON.stringify(value), appGroup, options, errorCode => {
         if (errorCode != null) {
           reject(errorCode)
         } else {
@@ -48,39 +76,39 @@ export default class SharedGroupPreferences {
       })
     })
   }
-/*
-  static async saveFile(filenameAndKey, urlToFile, appGroup, inputOptions) {
-    return new Promise((resolve, reject)=>{
-      if ((Platform.OS != 'ios') && (Platform.OS != 'android')) {
-        reject(Platform.OS)
-      }
-
-      const options = inputOptions || {}
-      RNReactNativeSharedGroupPreferences.saveFile(filenameAndKey, urlToFile, appGroup, options, errorCode=>{
-        if (errorCode != null) {
-          reject(errorCode)
-        } else {
-          resolve()
+  /*
+    static async saveFile(filenameAndKey, urlToFile, appGroup, inputOptions) {
+      return new Promise((resolve, reject)=>{
+        if ((Platform.OS != 'ios') && (Platform.OS != 'android')) {
+          reject(Platform.OS)
         }
+  
+        const options = inputOptions || {}
+        RNReactNativeSharedGroupPreferences.saveFile(filenameAndKey, urlToFile, appGroup, options, errorCode=>{
+          if (errorCode != null) {
+            reject(errorCode)
+          } else {
+            resolve()
+          }
+        })
       })
-    })
-  }
-
-  static async getUrlToFile(filenameAndKey, appGroup, inputOptions) {
-    return new Promise((resolve, reject)=>{
-      if ((Platform.OS != 'ios') && (Platform.OS != 'android')) {
-        reject(Platform.OS)
-      }
-
-      const options = inputOptions || {}
-      RNReactNativeSharedGroupPreferences.getUrlToFile(filenameAndKey, appGroup, options, (errorCode, item)=>{
-        if (errorCode != null) {
-          reject(errorCode)
-        } else {
-          resolve(JSON.parse(item))
+    }
+  
+    static async getUrlToFile(filenameAndKey, appGroup, inputOptions) {
+      return new Promise((resolve, reject)=>{
+        if ((Platform.OS != 'ios') && (Platform.OS != 'android')) {
+          reject(Platform.OS)
         }
+  
+        const options = inputOptions || {}
+        RNReactNativeSharedGroupPreferences.getUrlToFile(filenameAndKey, appGroup, options, (errorCode, item)=>{
+          if (errorCode != null) {
+            reject(errorCode)
+          } else {
+            resolve(JSON.parse(item))
+          }
+        })
       })
-    })
-  }
-  */
+    }
+    */
 }
